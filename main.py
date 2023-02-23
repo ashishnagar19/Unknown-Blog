cce86270a50902ff32c93f7385a2f62e14130416
ashishnagar19 <ashishnagar7042@gmail.com>
2023-02-24 00:26:19 +0530

    Full Fledged Blog Website Created Using Python(Flask)

--- main.py
+++ main.py
@@ -191,7 +191,7 @@ def add_new_post():
     return render_template("make-post.html", form=form, loggedin=current_user.is_authenticated, current_user=current_user)
 
 
-@app.route("/edit-post/<int:post_id>")
+@app.route("/edit-post/<int:post_id>", methods=['GET', 'POST'])
 @login_required
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
